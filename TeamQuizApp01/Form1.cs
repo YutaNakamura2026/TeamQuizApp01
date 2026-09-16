c0eb0f8d68049614667be18ef1a1a7321d1c86df
YutaNakamura2026 <uni202607_06@telework.to>
2026-09-16 15:08:36 +0900

    更新

--- TeamQuizApp01/Form1.cs
+++ TeamQuizApp01/Form1.cs
@@ -9,12 +9,52 @@ using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace TeamQuizApp01
-{
+{ 
     public partial class Form1 : Form
     {
+        private readonly QuestionLoader loader;
+        private readonly AnswerChecker checker;
+        private readonly ScoreManager score;
+        private readonly UiUpdater ui;
+
+        private Question current;
+
         public Form1()
         {
             InitializeComponent();
+
+            loader = new QuestionLoader();
+            checker = new AnswerChecker();
+            score = new ScoreManager();
+            ui = new UiUpdater(questionLabel,
+                new[] { answerButton1, answerButton2, answerButton3, answerButton4 },
+                logListBox);
+
+            LoadNextQuestion();
+        }
+
+        private void answerButton_Click(object sender, EventArgs e)
+        {
+            var btn = sender as Button;
+            int index = Array.IndexOf(new[] { answerButton1, answerButton2, answerButton3, answerButton4 }, btn);
+
+            bool result = checker.CheckAnswer(current, index);
+            score.Record(result);
+
+            ui.LogResult(result ? "正解！" : "不正解...");
+            ui.LogResult(score.GetResult());
+
+            LoadNextQuestion();
+        }
+
+        private void LoadNextQuestion()
+        {
+            current = loader.GetRandomQuestion();
+            ui.ShowQuestion(current);
         }
     }
 }
+
+
+
+
